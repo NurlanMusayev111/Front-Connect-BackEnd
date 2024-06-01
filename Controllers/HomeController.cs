52e4a398c3169456126098a18dc29db8e1edaa96
nurlanmusayev <nurlan.musayev.2002@inbox.ru>
2024-06-02 02:24:11 +0400

    FrontToBack

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FrontEndProject_Connect_BackEnd.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace FrontEndProject_Connect_BackEnd.Models
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
